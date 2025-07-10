57ef0a8dccecdb29a5e0ffbfd6a3b545336c7e87
sebaslade <sebaslade29@gmail.com>
2025-07-10 09:44:01 -0500

    feat: Agregando el login

--- src/main/java/com/init/APIRestIsilFitnessSAC/rest/ClienteRest.java
+++ src/main/java/com/init/APIRestIsilFitnessSAC/rest/ClienteRest.java
@@ -10,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.init.APIRestIsilFitnessSAC.entity.Cliente;
 import com.init.APIRestIsilFitnessSAC.repository.ClienteRepository;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/clientes")
 public class ClienteRest {
@@ -37,13 +40,6 @@ public class ClienteRest {
         return ResponseEntity.ok(cliente);
     }
 
-    //POST
-    @RequestMapping
-    public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) {
-        Cliente savedCliente = clienteRepository.save(cliente);
-        return ResponseEntity.ok(savedCliente);
-    }
-
     //PUT
     @PutMapping
     public ResponseEntity<Cliente> updateCliente(@RequestBody Cliente cliente) {
@@ -62,4 +58,25 @@ public class ClienteRest {
         clienteRepository.deleteById(id_cliente);
         return ResponseEntity.ok(null);
     }
+
+    //GET By Email and Password => Iniciar sesión
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Cliente request) {
+        Cliente clienteExistente = clienteRepository.findByEmail(request.getEmail());
+
+        if (clienteExistente == null) {
+            return ResponseEntity.status(404).body("El usuario no está registrado");
+        }
+
+        Cliente cliente = clienteRepository.findByEmailAndPassword(request.getEmail(), request.getPassword());
+
+        if (cliente == null) {
+            return ResponseEntity.status(401).body("Contraseña incorrecta");
+        }
+
+        cliente.setPassword(null); // opcional: ocultar contraseña
+
+        return ResponseEntity.ok(cliente);
+    }
+    
 }
