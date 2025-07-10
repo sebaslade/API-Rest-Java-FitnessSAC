aa14fae783ed27c15d47a1971aac96ac40d9fb81
sebaslade <sebaslade29@gmail.com>
2025-07-09 20:12:07 -0500

    feat: Modalidad Controller

--- src/main/java/com/init/APIRestIsilFitnessSAC/repository/EntrenamientoRepository.java
+++ src/main/java/com/init/APIRestIsilFitnessSAC/repository/EntrenamientoRepository.java
@@ -1,5 +1,11 @@
 package com.init.APIRestIsilFitnessSAC.repository;
 
-public interface EntrenamientoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.init.APIRestIsilFitnessSAC.entity.Entrenamiento;
+
+@Repository
+public interface EntrenamientoRepository extends JpaRepository<Entrenamiento, Integer> {
+    Entrenamiento findById(int id_entrenamiento);
 }
--- src/main/java/com/init/APIRestIsilFitnessSAC/rest/ModalidadRest.java
+++ src/main/java/com/init/APIRestIsilFitnessSAC/rest/ModalidadRest.java
@@ -1,5 +1,36 @@
 package com.init.APIRestIsilFitnessSAC.rest;
 
-public class ModalidadRest {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.init.APIRestIsilFitnessSAC.entity.Modalidad;
+import com.init.APIRestIsilFitnessSAC.repository.ModalidadRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/modalidades")
+public class ModalidadRest {
+    @Autowired
+    private ModalidadRepository modalidadRepository;
+
+    //GET
+    @GetMapping
+    public ResponseEntity<List<Modalidad>> getModalidades() {
+        List<Modalidad> modalidades = modalidadRepository.findAll();
+        return ResponseEntity.ok(modalidades);
+    }
+
+    //GET By Id
+    @RequestMapping(value="{modalidadId}")
+    public ResponseEntity<Modalidad> getModalidadById(@PathVariable("modalidadId") int id_modalidad) {
+        Modalidad modalidad = modalidadRepository.findById(id_modalidad);
+        return ResponseEntity.ok(modalidad);
+    }
+    
 }
