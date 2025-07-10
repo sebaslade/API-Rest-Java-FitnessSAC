6ff097978370fdbe36e59578b356609e7367fe87
sebaslade <sebaslade29@gmail.com>
2025-07-10 00:50:14 -0500

    feat: Habilitando CORS

--- src/main/java/com/init/APIRestIsilFitnessSAC/rest/EntrenamientoRest.java
+++ src/main/java/com/init/APIRestIsilFitnessSAC/rest/EntrenamientoRest.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.init.APIRestIsilFitnessSAC.entity.Entrenamiento;
 import com.init.APIRestIsilFitnessSAC.repository.EntrenamientoRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/entrenamientos")
 public class EntrenamientoRest {
