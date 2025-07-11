976550e56b4cb3b7eb25d674a19c9a1c83713e59
sebaslade <sebaslade29@gmail.com>
2025-07-10 20:28:22 -0500

    feat: deploy del proyecto

---
+++ src/main/java/com/init/APIRestIsilFitnessSAC/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.init.APIRestIsilFitnessSAC.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://fitnesssac-qip31p5z2-sebaslades-projects.vercel.app")
+            .allowedMethods("*")
+            .allowedHeaders("*");
+    }
+}
