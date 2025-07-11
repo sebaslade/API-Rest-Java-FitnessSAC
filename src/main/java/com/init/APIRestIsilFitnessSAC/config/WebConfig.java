0433d9525674de8b6bb2ad2579f27516d132beae
sebaslade <sebaslade29@gmail.com>
2025-07-10 21:27:06 -0500

    fix: Nueva conexion

--- src/main/java/com/init/APIRestIsilFitnessSAC/config/WebConfig.java
+++ src/main/java/com/init/APIRestIsilFitnessSAC/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://fitnesssac-qip31p5z2-sebaslades-projects.vercel.app")
+            .allowedOrigins("https://fitnesssac-qip31p5z2-sebaslades-projects.vercel.app",
+                "https://fitnesssac.vercel.app")
             .allowedMethods("*")
             .allowedHeaders("*");
     }
